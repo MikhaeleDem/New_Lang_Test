a110db4b82bc53fb30f029dc7549447e6b95fff5
Mikhaele <m.dannicov@mail.ru>
2021-05-25 01:09:54 +0300

    Отдельный репозиторий с работающим запросом

--- test_items.py
+++ test_items.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -8,6 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_assert_button_diff_language(browser):
     browser.get(link)
-    time.sleep(5)
+    # time.sleep(30)
     # Проверяем, что элемент присутствует на странице
-    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#add_to_basket_form")))
+    button = browser.find_elements_by_id("add_to_basket_form")
+    assert button, "Кнопка не найдена"
